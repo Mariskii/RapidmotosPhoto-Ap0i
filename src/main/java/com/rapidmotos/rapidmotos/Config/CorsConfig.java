b0bad35ed2ff1ab1e49af23b398c6600be025abe
Vicent Paya <vicentpayaginer@gmail.com>
2024-10-20 20:24:21 +0200

    feat: chanche allowed origins

--- src/main/java/com/rapidmotos/rapidmotos/Config/CorsConfig.java
+++ src/main/java/com/rapidmotos/rapidmotos/Config/CorsConfig.java
@@ -11,8 +11,9 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*");
+                .allowedOrigins("http://localhost:8100")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
