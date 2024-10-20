bf763eda5d1c90fe1d09a96f725bea177fe76d9c
Vicent Paya <vicentpayaginer@gmail.com>
2024-10-20 21:15:32 +0200

    fix: cors config

--- src/main/java/com/rapidmotos/rapidmotos/Dropbox/DropboxController.java
+++ src/main/java/com/rapidmotos/rapidmotos/Dropbox/DropboxController.java
@@ -2,15 +2,13 @@ package com.rapidmotos.rapidmotos.Dropbox;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/dropbox")
 public class DropboxController {
     @Autowired
