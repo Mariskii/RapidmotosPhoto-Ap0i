a43ded3a767d323f1d45c9b79c9a55fa9764cd58
Vicent Paya <vicentpayaginer@gmail.com>
2024-10-20 21:56:47 +0200

    fix: cors config

--- src/main/java/com/rapidmotos/rapidmotos/Config/CorsConfig.java
+++ src/main/java/com/rapidmotos/rapidmotos/Config/CorsConfig.java
@@ -2,17 +2,16 @@ package com.rapidmotos.rapidmotos.Config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer  {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Permite todas las rutas
-                .allowedOrigins("http://localhost:8100") // Cambia esto por la URL de tu app Ionic
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+        registry.addMapping("/**")
+                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
     }
 }
