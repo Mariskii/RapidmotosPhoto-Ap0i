83ccc7c2caeddf2e6d944e3cadc405ebe97e094f
Vicent Paya <vicentpayaginer@gmail.com>
2024-10-20 21:19:30 +0200

    fix: cors config

--- src/main/java/com/rapidmotos/rapidmotos/Users/UserController.java
+++ src/main/java/com/rapidmotos/rapidmotos/Users/UserController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@CrossOrigin
 @RequestMapping("/api/auth")
 public class UserController {
 
